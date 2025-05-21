c873773f20ac743727dc7d6e2e906a676b1dcecd
Rohan402-mern <rohanpanday84548@gmail.com>
2025-05-21 17:26:11 +0530

    dashboard route Completed!

--- src/Components/WorldCoffee/WorldCoffee.jsx
+++ src/Components/WorldCoffee/WorldCoffee.jsx
@@ -3,9 +3,20 @@ import './WorldCoffee.css'
 const WorldCoffee = () => {
   return (
     <div className='worldcoffee'>
+      <div className='worldcoffee-1'>
       <h1>Learn more about the world of coffee!</h1>
+      <p>Discover more</p>
+      </div>
       <div className="worldcoffee-cont">
-        
+        <div className="worldcoffee-wrapper">
+         <img src="https://preprodtsbstorage.blob.core.windows.net/cms/uploads/ICW_Live_Event_Day5_41f11ca3d2.jpg" alt="" />
+      <div className="worldcoffee-cont-1">
+        <button className='first-btn'>Coffee Culture</button>
+        <h1>Art & Science Of Coffee Brewing</h1>
+        <p>Master the perfect brew with Starbucks! Learn the art and science of coffee brewing.</p>
+        <button className='sec-btn'>Learn More</button>
+      </div>
+      </div>
       </div>
     </div>
   )
